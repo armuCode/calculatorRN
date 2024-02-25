f7516b6765b2daabb4fe96b3fd3215f71458e2f3
armucode <armucode@gmail.com>
2024-02-25 07:12:20 -0500

    first commit

---
+++ src/screens/CalculadoraScreen.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import {Text, View} from 'react-native';
+import {styles} from '../theme/appTheme';
+
+const CalculadoraScreen = () => {
+  return (
+    <View>
+      <Text style={styles.texto}>CalculadoraScreen</Text>
+    </View>
+  );
+};
+
+export default CalculadoraScreen;
